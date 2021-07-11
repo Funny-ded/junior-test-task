a3e1ac6217f3dc854d62061fe5ab24dca355d66f
Яшин Прохор <iashin.pa@phystech.edu>
2021-07-11 15:20:22 +0300

    Fixes

--- test-task/action/action.php
+++ test-task/action/action.php
@@ -1,34 +1,20 @@
 <?php
     // database connection
     $con = include('../config/db_connect.php');
-    // maximum length of note
-    $max_note_len = 255;
 
-    // requests functions
-    include('./requests/fetch.php');
-    include('./requests/add.php');
-    include('./requests/update.php');
-    include('./requests/delete.php');
+    // response class
+    include('./classes/response.php');
 
     $mysqli = create_connection($con['hostname'], $con['username'], $con['password'], $con['db']);
 
     // receive data from post request from main.js
-    $received_data = json_decode(file_get_contents('php://input'));
+    $_POST = json_decode(file_get_contents('php://input'), true);
 
-    // actions with request
-    if ($received_data->action == 'fetchAll'){
-        // fetch all data from the database
-        fetch_all($mysqli);
-    } else if($received_data->action == 'addNote') {
-        // add data to the database
-        add_note($received_data->body, $max_note_len, $mysqli);
-    } else if($received_data->action == 'updateData'){
-        // update data in the database
-      update_note($received_data->id, $received_data->body, $max_note_len, $mysqli);
-    } else if ($received_data->action == 'deleteData') {
-      // delete data from the database
-      delete_note($received_data->id, $mysqli);
-    }
+    $response = new Response($mysqli);
+
     // close connection
     $mysqli->close();
+    // send data back
+    $response->send_response();
+
 ?>
