a96e8d5c8dd727de4bfc80ec5e6064dc0f1ae05b
Яшин Прохор <iashin.pa@phystech.edu>
2021-07-11 11:49:38 +0300

    Fixes

--- test-task/src/components/adder.vue
+++ test-task/src/components/adder.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="new-note">
     <label for="new-note">Add new note now</label>
-    <textarea v-model="newNote" v-on:keydown.enter.prevent="addNote"></textarea>
+    <textarea v-model="newNote" v-on:keydown.enter="addNote" ></textarea>
     <p class="error-message" v-if="error.add">{{ error.add }}</p>
   </div>
 </template>
@@ -19,31 +19,34 @@ export default {
     }
   },
   methods:{
-    addNote: function(){
-      // send request to PHP script to add note
-      axios({
-        method: "POST",
-        url: "./action/action.php",
-        data: {
-          action: "addNote",
-          body: this.newNote
-        }
-      }).then(response => {
-        // check if it is not success
-        if (response.data.status === 'success'){
-          // update data
-          this.$parent.$children[1].fetchData();
-          this.error.add = '';
-          this.newNote = "";
+    addNote: function(evt) {
+      if (!evt.shiftKey) {
+        evt.preventDefault();
+        // send request to PHP script to add note
+        axios({
+          method: "POST",
+          url: "./action/action.php",
+          data: {
+            action: "addNote",
+            body: this.newNote
+          }
+        }).then(response => {
+          // check if it is not success
+          if (response.data.status === 'success') {
+            // update data
+            this.$root.$emit('fetchAll');
+            this.error.add = '';
+            this.newNote = "";
 
-        } else {
-          // create an error message
-          throw response.data.send;
-        }
-      })
-        .catch(error => {
-          this.error.add = error;
-        });
+          } else {
+            // create an error message
+            throw response.data.send;
+          }
+        })
+          .catch(error => {
+            this.error.add = error;
+          });
+    }
     },
   }
 }
